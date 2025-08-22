85881b24f772ecc5bcbfc7d8f7ef7aa8b030c721
Byte <bytehash69@gmail.com>
2025-08-22 15:59:45 +0530

    Ix-Loan & Repay

--- programs/anchor-flash-loans/src/lib.rs
+++ programs/anchor-flash-loans/src/lib.rs
@@ -17,10 +17,115 @@ pub mod anchor_flash_loans {
     use super::*;
 
     pub fn borrow(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
+        require!(borrow_amount > 0, ProtocolError::InvalidAmount);
+
+        let seeds = &[b"protocol".as_ref(), &[ctx.bumps.protocol]];
+        let signer_seeds: &[&[&[u8]]] = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                authority: ctx.accounts.protocol.to_account_info(),
+                from: ctx.accounts.protocol_ata.to_account_info(),
+                to: ctx.accounts.borrower_ata.to_account_info(),
+            },
+            signer_seeds,
+        );
+
+        transfer(cpi_ctx, borrow_amount)?;
+
+        let ixs = ctx.accounts.instructions.to_account_info();
+
+        let instruction_sysvar = ixs.try_borrow_data()?;
+        let len = u16::from_be_bytes(instruction_sysvar[0..2].try_into().unwrap());
+
+        msg!("Total ixs in this tx: {}", len);
+
+        let mut found_repay = false;
+
+        for i in 1..len {
+            if let Ok(repay_ix) = load_instruction_at_checked(i as usize, &ixs) {
+                msg!("Checking instruction at index {}", i);
+                msg!("Program ID: {}", repay_ix.program_id);
+
+                if repay_ix.program_id == ID
+                    && repay_ix.data.len() >= 8
+                    && repay_ix.data[0..8].eq(instruction::Repay::DISCRIMINATOR)
+                {
+                    msg!("Found repay instruction at index: {}", i);
+
+                    if repay_ix.accounts.len() >= 5 {
+                        let repay_borrower_ata = repay_ix
+                            .accounts
+                            .get(3)
+                            .ok_or(ProtocolError::InvalidBorrowerAta)?;
+                        require_keys_eq!(
+                            repay_borrower_ata.pubkey,
+                            ctx.accounts.borrower_ata.key(),
+                            ProtocolError::InvalidBorrowerAta
+                        );
+
+                        let repay_protocol_ata = repay_ix
+                            .accounts
+                            .get(4)
+                            .ok_or(ProtocolError::InvalidProtocolAta)?;
+                        require_keys_eq!(
+                            repay_protocol_ata.pubkey,
+                            ctx.accounts.protocol_ata.key(),
+                            ProtocolError::InvalidProtocolAta
+                        );
+
+                        found_repay = true;
+                        break;
+                    } else {
+                        msg!(
+                            "Repay instruction has insufficient accounts: {}",
+                            repay_ix.accounts.len()
+                        );
+                    }
+                }
+            }
+        }
+
+        require!(found_repay, ProtocolError::MissingRepayIx);
+
         Ok(())
     }
 
     pub fn repay(ctx: Context<Loan>) -> Result<()> {
+        let ixs = ctx.accounts.instructions.to_account_info();
+
+        let mut amount_borrowed: u64;
+
+        if let Ok(borrow_ix) = load_instruction_at_checked(0, &ixs) {
+            let mut borrowed_data: [u8; 8] = [0u8; 8];
+            borrowed_data.copy_from_slice(&borrow_ix.data[8..16]);
+            amount_borrowed = u64::from_le_bytes(borrowed_data)
+        } else {
+            return Err(ProtocolError::MissingBorrowIx.into());
+        }
+
+        let fee = (amount_borrowed as u128)
+            .checked_mul(500)
+            .unwrap()
+            .checked_div(10_000)
+            .ok_or(ProtocolError::Overflow)? as u64;
+        amount_borrowed = amount_borrowed
+            .checked_add(fee)
+            .ok_or(ProtocolError::Overflow)?;
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.borrower_ata.to_account_info(),
+                    to: ctx.accounts.protocol_ata.to_account_info(),
+                    authority: ctx.accounts.borrower.to_account_info(),
+                },
+            ),
+            amount_borrowed,
+        )?;
+
         Ok(())
     }
 }
@@ -39,7 +144,7 @@ pub struct Loan<'info> {
     pub mint: Account<'info, Mint>,
 
     #[account(
-        init_if_needed, 
+        init_if_needed,
         payer = borrower,
         associated_token::mint = mint,
         associated_token::authority = borrower
@@ -53,7 +158,7 @@ pub struct Loan<'info> {
 
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
 #[error_code]
